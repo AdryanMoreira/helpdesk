08359a98a23c4e92bfde2f28014c0df5742d274a
AdryanMoreira <adriano.moreira@totvs.com.br>
2023-06-28 22:11:16 -0300

    componete tecnico create criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-
 import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
 import { AuthInterceptorProvider } from './interceptores/auth.interceptor';
+import { TecnicoCreateComponent } from './components/tecnicos/tecnico-create/tecnico-create.component';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,8 @@ import { AuthInterceptorProvider } from './interceptores/auth.interceptor';
     HomeComponent,
     HeaderComponent,
     TecnicoListComponent,
-    LoginComponent
+    LoginComponent,
+    TecnicoCreateComponent
   ],
   imports: [
     BrowserModule,
